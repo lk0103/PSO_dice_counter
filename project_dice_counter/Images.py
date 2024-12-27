fd8e81438eec04d4489c1df74f7dc71b2aa80cb9
LK0103 <korbelova19@uniba.sk>
2024-12-27 12:57:29 +0100

    changed dir name

--- project_dice_counter/Images.py
+++ project_dice_counter/Images.py
@@ -6,10 +6,10 @@ from matplotlib import pyplot as plt
 class Images:
     def __init__(self):
         self.val_dir = "./database/validation_dataset/"
-        self.val_images = self.list_files(self.directory_database)
+        self.val_images = self.list_files(self.val_dir)
 
         self.test_dir = "./database/test_dataset/"
-        self.test_images = self.list_files(self.directory_banknotes)
+        self.test_images = self.list_files(self.test_dir)
 
     def list_files(self, directory):
         files = []
