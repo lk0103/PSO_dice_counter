4c006addeb511f28e90728f64933b2168f66110e
LK0103 <korbelova19@uniba.sk>
2024-12-27 12:54:02 +0100

    image class

---
+++ project_dice_counter/Images.py
@@ -0,0 +1,25 @@
+import os
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Images:
+    def __init__(self):
+        self.val_dir = "./database/validation_dataset/"
+        self.val_images = self.list_files(self.directory_database)
+
+        self.test_dir = "./database/test_dataset/"
+        self.test_images = self.list_files(self.directory_banknotes)
+
+    def list_files(self, directory):
+        files = []
+        for filename in os.listdir(directory):
+            if os.path.isfile(os.path.join(directory, filename)):
+                files.append(filename)
+        return files
+
+    def show(self, img, title=None):
+        plt.imshow(img[:, :, ::-1])
+        plt.title(title)
+        plt.axis('off')
+        plt.show()
