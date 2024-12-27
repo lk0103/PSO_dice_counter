b94df2d624c8877e64a662639aa27fc1f0192c7c
LK0103 <korbelova19@uniba.sk>
2024-12-27 13:01:06 +0100

    changes

--- project_dice_counter/Images.py
+++ project_dice_counter/Images.py
@@ -5,10 +5,10 @@ from matplotlib import pyplot as plt
 
 class Images:
     def __init__(self):
-        self.val_dir = "./database/validation_dataset/"
+        self.val_dir = "./dataset/validation_dataset/"
         self.val_images = self.list_files(self.val_dir)
 
-        self.test_dir = "./database/test_dataset/"
+        self.test_dir = "./dataset/test_dataset/"
         self.test_images = self.list_files(self.test_dir)
 
     def list_files(self, directory):
@@ -23,3 +23,8 @@ class Images:
         plt.title(title)
         plt.axis('off')
         plt.show()
+
+print(Images().val_images)
+print(Images().test_images)
+print(Images().val_dir)
+print(Images().test_dir)
